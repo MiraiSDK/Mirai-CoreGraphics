a0840689dd60ac4428a91b0c7aafcb42a65e4e06
Yonghui Chen <chyhfj@gmail.com>
2015-03-31 00:11:06 +0800

    support x86

---
+++ toolchain_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+checkError()
+{
+    if [ "${1}" -ne "0" ]; then
+        echo "*** Error: ${2}"
+        exit ${1}
+    fi
+}
+
+if [ ! -f $MIRAI_SDK_PREFIX/lib/libCoreGraphics.so ]; then
+	echo "build CoreGraphics..."
+	pushd $MIRAI_PROJECT_ROOT_PATH/Mirai-CoreGraphics
+	xcodebuild -target CoreGraphics-Android -xcconfig xcconfig/Android-$ABI.xcconfig clean
+	
+	xcodebuild -target CoreGraphics-Android -xcconfig xcconfig/Android-$ABI.xcconfig
+	checkError $? "build CoreGraphics failed"
+	
+	#clean up
+	rm -r build
+	popd
+fi
